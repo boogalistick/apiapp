b6222803c48fa196eda17423da9de543140e88a4
Andrei Pirjol <boogalistick@users.noreply.github.com>
2022-03-09 22:23:20 +0000

    Create inputs.tf

---
+++ inputs.tf
@@ -0,0 +1,25 @@
+variable "access_key" {
+    default = "****"
+}
+variable "secret_key" {
+    default = "*****"
+}
+variable "region" {
+    default = "eu-west-1"
+}
+variable "bucket" {
+    default = "bucket"
+}
+variable "aws_ami" {
+    default = "ami-00798d7180f25aac2"
+}
+variable "aws_type" {
+    default = "t2.micro"
+}
+// Other Configuration
+variable "workspace" {
+    default = "user"
+}
+variable "password" {
+    default = "*****"
+}
